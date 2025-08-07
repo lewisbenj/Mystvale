cef7b3bde73c6b065fde8f7691d51a45e53631e9
Nguyễn Võ Chí Nguyện <nguyenchie60@gmail.com>
2025-08-07 08:03:54 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,19 +5,19 @@ module.exports = {
   language: "vi",
   ownerID: ["812279245661274132", ""], 
   mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  spotifyClientId : "76843c9f591142039cc9e48664b11745",
+  spotifyClientSecret : "961bddea32f0455381bce0afaea3fe99",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/Mystvale",
+  SupportServer: "https://discord.gg/mystvale",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
+      name: "Mystvale",
       password: "glaceyt",
       host: "5.39.63.207",
       port:  8262,
