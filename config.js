c3dbcfb1a926f6b95627149d232d7cc19fa49673
Nguyễn Võ Chí Nguyện <nguyenchie60@gmail.com>
2025-08-09 21:39:25 -0500

    Update config.js
    Signed-off-by: Nguyễn Võ Chí Nguyện <nguyenchie60@gmail.com>

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "Mystvale",
+      name: "GlaceYT",
       password: "glaceyt",
       host: "5.39.63.207",
       port:  8262,
